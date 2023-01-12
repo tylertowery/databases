03784450597515028b5f9e68a6856ae4c685b74f
Tyler Towery <tylertowery21@gmail.com>
2023-01-11 17:13:05 -0800

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,13 +1,26 @@
+DROP DATABASE chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE users (
+  ID int AUTO_INCREMENT,
+  NAME varchar(50),
+  PRIMARY KEY (ID)
+);
+
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  ID int AUTO_INCREMENT,
+  TEXT varchar(255),
+  USER int,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user) REFERENCES users(id)
 );
 
 /* Create other tables and define schemas for them here! */
-
 
 
 
